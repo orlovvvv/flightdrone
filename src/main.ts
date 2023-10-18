32c3e6873d96de3ba0e2adb12e4ab6a206f16f59
orlovvvv <89607019+orlovvvv@users.noreply.github.com>
2023-10-18 15:53:10 +0000

    ci(routing): :rewind: revert to use ion router, add http
    Reverse and use Ionic router and routing strategy, add HttpClient to app.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
-import { enableProdMode, importProvidersFrom } from '@angular/core';
+import { enableProdMode } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
-import { provideRouter } from '@angular/router';
-import { provideIonicAngular } from '@ionic/angular/standalone';
-
+import { RouteReuseStrategy, provideRouter } from '@angular/router';
+import {
+  IonicRouteStrategy,
+  provideIonicAngular,
+} from '@ionic/angular/standalone';
+import { provideHttpClient } from '@angular/common/http';
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
 import { environment } from './environments/environment';
@@ -12,5 +15,10 @@ if (environment.production) {
 }
 
 bootstrapApplication(AppComponent, {
-  providers: [provideIonicAngular(), provideRouter(routes)],
+  providers: [
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    provideIonicAngular(),
+    provideRouter(routes),
+    provideHttpClient(),
+  ],
 });
