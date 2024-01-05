6154e54d33e69d9da2327441343d72df312dacff
orlovvvv <89607019+orlovvvv@users.noreply.github.com>
2024-01-05 12:21:03 +0000

    fix(functions): :refactor: rewrite entire function

--- functions/createProfile/src/main.js
+++ functions/createProfile/src/main.js
@@ -1,26 +1,49 @@
 // Perform all your imports
-const sdk = require("node-appwrite");
+import { Client, Databases } from "node-appwrite";
 
-module.exports = async function (req, res) {
-  const client = new sdk.Client();
-  if (
-    !process.env.APPWRITE_FUNCTION_PROJECT_ID ||
-    !process.env.APPWRITE_API_KEY ||
-    !process.env.DATABASE_ID ||
-    !process.env.DATABASE_COLLECTION
-  ) {
-    console.warn(
-      "Environment variables are not set. Function cannot use Appwrite SDK.",
-    );
-  } else {
-    client
-      .setEndpoint("https://cloud.appwrite.io/v1")
-      .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
-      .setKey(process.env.APPWRITE_API_KEY);
+// This is your Appwrite function
+// It's executed each time we get a request
+export default async ({ req, res, log, error }) => {
+  // Why not try the Appwrite SDK?
+  //
+  // const client = new Client()
+  //    .setEndpoint('https://cloud.appwrite.io/v1')
+  //    .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
+  //    .setKey(process.env.APPWRITE_API_KEY);
+
+  // You can log messages to the console
+  log("Hello, Logs!");
+
+  // If something goes wrong, log an error
+  error("Hello, Errors!");
+
+  // The `req` object contains the request data
+  if (req.method === "GET") {
+    // Send a response with the res object helpers
+    // `res.send()` dispatches a string back to the client
+    return res.send("Hello, World!");
   }
 
-  const database = new sdk.Databases(client);
-  try {
+  if (req.method === "POST") {
+    const client = new Client();
+    if (
+      !process.env.APPWRITE_FUNCTION_PROJECT_ID ||
+      !process.env.APPWRITE_API_KEY ||
+      !process.env.DATABASE_ID ||
+      !process.env.DATABASE_COLLECTION
+    ) {
+      console.warn(
+        "Environment variables are not set. Function cannot use Appwrite SDK.",
+      );
+    } else {
+      client
+        .setEndpoint("https://cloud.appwrite.io/v1")
+        .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
+        .setKey(process.env.APPWRITE_API_KEY);
+    }
+
+    const database = new Databases(client);
+
     const userId = req.body.$id; // Assuming the user ID is in the request body
 
     const profileData = {
@@ -35,10 +58,14 @@ module.exports = async function (req, res) {
       profileData,
     );
 
-    return res.json({
-      profile: "Profile created successfully",
-    });
-  } catch (error) {
-    return res.json({ error: "Internal Server Error" });
+    return res.send("Profile created successfully");
   }
+
+  // `res.json()` is a handy helper for sending JSON
+  return res.json({
+    motto: "Build like a team of hundreds_",
+    learn: "https://appwrite.io/docs",
+    connect: "https://appwrite.io/discord",
+    getInspired: "https://builtwith.appwrite.io",
+  });
 };
