318630f04d7f5393ae1509489006cceb9a39dfa9
orlovvvv <89607019+orlovvvv@users.noreply.github.com>
2024-01-05 11:49:17 +0000

    fix(functions): :bug: fix imports

--- functions/createProfile/src/main.js
+++ functions/createProfile/src/main.js
@@ -1,11 +1,10 @@
-import { Client, Databases } from "node-appwrite";
-
+const sdk = require("node-appwrite");
 // This is your Appwrite function
 // It's executed each time we get a request
 export default async ({ req, res, log, error }) => {
   // Why not try the Appwrite SDK?
   //
-  const client = new Client()
+  const client = new sdk.Client()
     .setEndpoint("https://cloud.appwrite.io/v1")
     .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
     .setKey(process.env.APPWRITE_API_KEY);
@@ -21,7 +20,7 @@ export default async ({ req, res, log, error }) => {
     // Send a response with the res object helpers
     // `res.send()` dispatches a string back to the client
     const userId = req.body.$id;
-    const database = new Databases(client);
+    const database = new sdk.Databases(client);
 
     try {
       if (req.method === "POST") {
