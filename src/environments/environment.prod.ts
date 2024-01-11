3f0d80a566109b50e68cedf217fb5efe403432df
orlovvvv <89607019+orlovvvv@users.noreply.github.com>
2024-01-11 16:29:46 +0000

    ci(ci/di): use dotenv for enviroment vars, bring back enviroment.ts

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  apiKey: process.env.API_KEY,
+  endpoint: process.env.ENDPOINT,
+  projectId: process.env.PORJECT_ID,
+  weatherApi: process.env.WEATHER_SERCRET,
+  databaseId: process.env.DATABASE_ID,
+  profileCollectionId: process.env.PROFILE_COLLECTION,
+  flightCollectionId: process.env.FLIGHT_COLLECTION,
+  droneCollectionId: process.env.DRONE_COLLECTION,
+  production: true,
+};
